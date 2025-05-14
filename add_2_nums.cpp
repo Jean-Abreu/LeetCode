81216cf8d9ee60fc8cd9d0bdae6d1a01b6243294
Jean Abreu <jean.abreu490@gmail.com>
2025-05-14 12:50:14 -0400

    Commented on the code, will begin performing problem solving.

---
+++ add_2_nums.cpp
@@ -0,0 +1,7 @@
+//Problem: Given two non-empty linked lists, created a third linked list representing the sum of each node. 
+//The data of the linked list will be stored in reverse order. 
+
+//For this problem, I have not considered using a brute force approach. So far, the solution I have seems to be the best iteration.
+//This will need to be tested and confirmed with said future tests.
+//But as for the solution, I created a third linked list and store the data as I add, this allows for a single swoop of performance.
+//I would say that the time complexity of my solution is O((m or n), depending on which linked list the most nodes. 
