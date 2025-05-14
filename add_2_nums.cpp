f6cb6880919a75f1192d5707107ee18b6a1bc6c5
Jean Abreu <jean.abreu490@gmail.com>
2025-05-14 12:52:10 -0400

    Created structure for the code

--- add_2_nums.cpp
+++ add_2_nums.cpp
@@ -1,3 +1,7 @@
+#include<iostream>
+#include<vector>
+
+
 //Problem: Given two non-empty linked lists, created a third linked list representing the sum of each node. 
 //The data of the linked list will be stored in reverse order. 
 
@@ -5,3 +9,12 @@
 //This will need to be tested and confirmed with said future tests.
 //But as for the solution, I created a third linked list and store the data as I add, this allows for a single swoop of performance.
 //I would say that the time complexity of my solution is O((m or n), depending on which linked list the most nodes. 
+
+class Solution {
+
+};
+
+int main(){
+
+    return 0;
+}
